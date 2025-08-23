e1c810b9419274b6723055eedc4fb4a88aad85e1
yyukar <fusuyrakuy@gmail.com>
2025-08-23 22:09:07 +0300

    assignment 6 revise added.

--- src/main/resources/week6/sqlfundamentals2/assignment6.sql
+++ src/main/resources/week6/sqlfundamentals2/assignment6.sql
@@ -8,11 +8,9 @@ FROM film
 WHERE title LIKE 'C%'
 
 --- film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
-SELECT length
+SELECT MAX(length)
 FROM film
 WHERE rental_rate = 0.99
-ORDER BY length DESC
-LIMIT 1
 
 --- film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
 SELECT COUNT(DISTINCT replacement_cost)
